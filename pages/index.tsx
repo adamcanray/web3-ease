9f3be1750e3297cd2dface50c5fe71d5b4326dd7
adamcanray <adamcool180@gmail.com>
2023-06-14 02:23:31 +0700

    update some UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import ParallaxNetworkContractAbi from "@/utils/ParallaxNetworkContractAbi.json"
 import { useEffect, useState } from "react";
 import { parseEther } from "viem";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const CONTRACT_ADDRESS = "0xe63434289AB72602f4b446e00e716206c9A9B97a";
 const NETWORK_CHAIN_ID = 11155111;
@@ -14,7 +15,7 @@ export default function Home() {
   const {
     connect,
     connectors,
-    error,
+    error: connectError,
     isLoading,
     pendingConnector,
     isError: connectIsError,
@@ -71,12 +72,21 @@ export default function Home() {
     getTotalSupply();
   }, []);
 
-  if (connectIsError) {
+  /**
+   * Renders this UI on connection error, unless the code equals 4001
+   */
+  if (
+    connectIsError &&
+    (connectError as Error & { code: number }).code !== 4001
+  ) {
     return (
       <div className="bg-gray-950 min-h-screen flex flex-col justify-center items-center">
         <p className="text-xl font-medium text-white">Something went wrong</p>
         <p className="text-base font-normal text-white">
           You maybe want to refresh this page
+        </p>
+        <p className="mt-2 text-sm font-normal text-red-500">
+          {connectError?.message}
         </p>
       </div>
     );
@@ -85,19 +95,40 @@ export default function Home() {
   if (isConnected)
     return (
       <div className="bg-gray-950">
-        <header className="flex justify-between items-center flex-col md:flex-row px-4 py-3 w-full max-w-5xl mx-auto">
+        <header className="flex justify-between items-center flex-col sm:flex-row px-4 py-3 w-full max-w-5xl mx-auto">
           <div className="">
-            <p className="text-xl font-semibold text-white">Web3Ease</p>
+            <Link
+              href={{ pathname: "/" }}
+              className="text-3xl font-semibold text-gradient-animate-1"
+            >
+              Web3Ease
+            </Link>
           </div>
           <button
             onClick={() => router.push("/profile")}
-            className="text-base font-normal text-white hover:text-gray-950 border rounded-full border-white hover:bg-white p-1 mt-2 md:mt-0"
+            className="text-base font-normal text-white hover:text-gray-950 border-2 rounded-full border-white hover:bg-white p-1 mt-2 md:mt-0 border-dashed"
           >
             {address ? (
-              <p>
-                {" "}
-                Wallet: {address.slice(0, 6)}...
-                {address.slice(-4)}{" "}
+              <p className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+                  />
+                </svg>
+                <span className="ml-1">Wallet: </span>
+                <span className="text-gradient-1">
+                  {address.slice(0, 6)}...
+                  {address.slice(-4)}{" "}
+                </span>
               </p>
             ) : (
               <p> Not connected </p>
@@ -105,20 +136,21 @@ export default function Home() {
           </button>
         </header>
         <div className="min-h-screen flex justify-center items-center px-4">
-          <div className="flex flex-col items-center w-full max-w-md">
+          <div className="flex flex-col items-center w-full max-w-md text-center">
             {/* eslint-disable-next-line */}
             <img src="/observing.gif" alt="observing gif" className="mx-auto" />
-            <div className="mt-4 border border-gray-100 w-full p-3">
+            <div className="border-x-2 border-b-2 border-gray-100 w-full p-3 border-dashed">
               <p className="text-lg font-normal text-white">Token Minted:</p>
-              <span className="mt-1 text-lg font-semibold">
+              <span className="mt-1 text-2xl font-semibold">
                 {mintedCount || "-"}
               </span>
             </div>
-            <div className="mt-8 w-full">
+            <div className="border-l-2 border-gray-100 border-dashed h-8"></div>
+            <div className="mt-2 w-full">
               <p className="text-white text-lg">To mint:</p>
               <input
                 type="number"
-                className="border-b border-b-gray-100 bg-transparent w-full p-2 text-lg focus:outline-none"
+                className="border-b-2 border-b-gray-100 bg-transparent w-full p-2 text-2xl focus:outline-none text-center border-dashed"
                 onChange={toMintCountOnChange}
                 value={toMintCount}
               />
@@ -127,13 +159,13 @@ export default function Home() {
               <p className="text-sm text-red-500 mb-1">{errorMessage}</p>
               {chain?.id !== NETWORK_CHAIN_ID && (
                 <p className="text-sm text-red-500 mb-1">
-                  Your not on Sepolia testnet
+                  You are not on Sepolia test network
                 </p>
               )}
               <button
                 disabled={chain?.id !== NETWORK_CHAIN_ID}
                 onClick={() => doMint()}
-                className="w-full px-2 py-1 rounded bg-indigo-500 hover:bg-indigo-600 disabled:opacity-80 disabled:cursor-not-allowed transition-colors"
+                className="w-full px-3 py-2 text-lg rounded-lg bg-gradient-1 disabled:opacity-80 disabled:cursor-not-allowed"
               >
                 Mint NFT {mintIsLoading ? "(loading..)" : ""}
               </button>
@@ -145,11 +177,14 @@ export default function Home() {
 
   return (
     <div className="bg-gray-950 min-h-screen flex justify-center items-center px-4">
-      <div className="text-center">
+      <div className="text-center w-full max-w-5xl mx-auto">
         {/* eslint-disable-next-line */}
         <img src="/mth.gif" alt="mth gif" className="mx-auto" />
-        <h1 className="mt-4 text-2xl font-bold text-white">
-          Welcome to Web3Ease!
+        <div className="flex justify-center">
+          <div className="border-l-2 border-gray-100 border-dashed h-8"></div>
+        </div>
+        <h1 className="mt-2 text-4xl font-bold text-white">
+          Welcome to <span className="text-gradient-animate-1">Web3Ease</span>!
         </h1>
         <p className="text-lg font-normal text-white">
           Your go-to Ethereum wallet for a hassle-free and intuitive experience
@@ -163,7 +198,7 @@ export default function Home() {
               onClick={() => {
                 connect({ connector });
               }}
-              className="px-2 py-1 rounded bg-orange-500 hover:bg-orange-600"
+              className="px-3 py-2 text-lg rounded-lg bg-orange-500 hover:bg-orange-600 disabled:opacity-80 disabled:cursor-not-allowed"
             >
               Connect with {connector.name}
               {!connector.ready && " (unsupported)"}
@@ -173,7 +208,11 @@ export default function Home() {
             </button>
           ))}
 
-          {error && <div>{error.message}</div>}
+          {connectError && (
+            <p className="mt-2 text-sm font-normal text-red-500">
+              {connectError.message}
+            </p>
+          )}
         </div>
       </div>
     </div>
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,10 +1,6 @@
-import { readContract } from "@wagmi/core";
 import { useAccount, useBalance, useDisconnect, useEnsName } from "wagmi";
-import ParallaxNetworkContractAbi from "@/utils/ParallaxNetworkContractAbi.json";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
-
-const CONTRACT_ADDRESS = "0xe63434289AB72602f4b446e00e716206c9A9B97a";
 
 export default function Home() {
   const router = useRouter();
@@ -24,7 +20,7 @@ export default function Home() {
       <div className="min-h-screen flex flex-col justify-center items-center relative w-full max-w-5xl mx-auto">
         <button
           onClick={() => router.push("/")}
-          className="absolute top-3 left-3 px-3 py-1 flex items-center justify-center rounded-full bg-gray-950 hover:bg-gray-900 active:bg-gray-900"
+          className="absolute top-3 left-3 px-3 py-2 flex items-center justify-center rounded-full bg-gray-950 hover:bg-gray-900 active:bg-gray-900"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -43,39 +39,45 @@ export default function Home() {
           <p className="ml-2">Back</p>
         </button>
 
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-56 h-56"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-            />
-          </svg>
-        </div>
-
-        <p className="text-base font-light text-white">Address</p>
-        <p className="text-base font-semibold text-white">
-          {ensName ?? address}
-        </p>
-        <p className="text-base font-light text-white">Balance</p>
-        <p className="text-base font-semibold text-white">
-          {balance?.formatted}
-        </p>
-
-        <div className="mt-8">
-          <button
-            className="px-2 py-1 rounded bg-red-500 hover:bg-red-600 transition-colors"
-            onClick={() => disconnect()}
-          >
-            Logout
-          </button>
+        <div className="text-center px-4">
+          <div className="rounded-full border-2 border-gray-100 border-dashed inline-block">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-56 h-56"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+          </div>
+          <div className="mt-6 border-y-2 border-gray-100 border-dashed text-center p-4">
+            <p className="text-sm md:text-base font-light text-white">
+              Address
+            </p>
+            <p className="text-sm md:text-base font-semibold text-gradient-1">
+              {ensName ?? address}
+            </p>
+            <p className="text-sm md:text-base font-light text-white">
+              Balance
+            </p>
+            <p className="text-sm md:text-base font-semibold text-gradient-2">
+              {balance?.formatted} {balance?.symbol}
+            </p>
+          </div>
+          <div className="mt-8 w-full">
+            <button
+              className="w-full px-3 py-2 rounded-lg bg-red-500 hover:bg-red-600 transition-colors text-base md:text-lg"
+              onClick={() => disconnect()}
+            >
+              Logout
+            </button>
+          </div>
         </div>
       </div>
     </div>
